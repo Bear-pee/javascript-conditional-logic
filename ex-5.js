306d5529740dabdf80e3a566112e5a3810806f42
Bear-pee <puringto5@gmail.com>
2024-09-13 02:51:19 +0700

    Update ex-5.js

--- ex-5.js
+++ ex-5.js
@@ -1,9 +1,9 @@
-function calculateStudentGrade(score) {if( score >= 0 && score <= 59){grade = "F"} 
-else if (score >= 60 && score <=69){grade = "D"} 
-else if (score >= 70 && score <= 79){grade = "C"} 
-else if (score>= 80 && score<= 89){grade = "B"} 
-else if (score>= 90 && score<= 100){grade = "A"}
-else {grade = "F"}
+function calculateStudentGrade(score) {if( score >= 0 && score <= 59){return "F"} 
+else if (score >= 60 && score <=69){return "D"} 
+else if (score >= 70 && score <= 79){return "C"} 
+else if (score>= 80 && score<= 89){return "B"} 
+else if (score>= 90 && score<= 100){return "A"}
+else {return"F"}
 };
 
 // Example case
