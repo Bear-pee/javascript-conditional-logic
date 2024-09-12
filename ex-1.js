46a822b5911b5b35f9235a28cf176b08cbf8fd7f
Bear-pee <puringto5@gmail.com>
2024-09-13 02:20:07 +0700

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,6 +1,6 @@
 //Exercise 1
 let lightBulbStatus = "On";
 
-if (lightBulbStatus = "On"){"Light bulb is On."}
-    else if (lightBulbStatus = "off"){"Light bulb is Off."};
+if (lightBulbStatus === "On"){console.log("Light bulb is On.")}
+    else if (lightBulbStatus === "off"){console.log("Light bulb is Off.")};
 console.log(lightBulbStatus);
